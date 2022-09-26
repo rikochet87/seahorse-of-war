ab3300931c4afb4211f016b05c0db435b6155d9c
matias <matias3282@hotmail.com>
2022-09-26 17:59:51 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**@type {HTMLCanvasElement}*/
+
+window.addEventListener("load", function(){
+    //canvas setup
+    const canvas = document.getElementById("canvas1");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 1500;
+    canvas.height = 500;
+})
