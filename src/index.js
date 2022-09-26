2f8d9f753d3a2f0c1d0ddaab6ae7bddf3194662d
matias <matias3282@hotmail.com>
2022-09-26 18:10:18 -0300

    Object Oriented programming with JavaScript

--- src/index.js
+++ src/index.js
@@ -6,4 +6,29 @@ window.addEventListener("load", function(){
     const ctx = canvas.getContext("2d");
     canvas.width = 1500;
     canvas.height = 500;
+
+    class InputHandler{
+
+    }
+    class Projectile{
+
+    }
+    class Particle{
+
+    }
+    class Enemy{
+
+    }
+    class Layer{
+
+    }
+    class Background{
+
+    }
+    class UI{
+
+    }
+    class Game{
+
+    }
 })
