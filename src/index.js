5e1d48264c9ffb5d61d07688fb623310e880d02f
matias <matias3282@hotmail.com>
2022-09-26 18:33:37 -0300

    create player

--- src/index.js
+++ src/index.js
@@ -16,6 +16,22 @@ window.addEventListener("load", function(){
     class Particle{
 
     }
+    class Player{
+        constructor(game){
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+        }
+        update(){
+            this.y += this.speedY;
+        }
+        draw(context){
+            context.fillRect(this.x, this.y, this.width, this.height)
+        }
+    }
     class Enemy{
 
     }
@@ -29,6 +45,17 @@ window.addEventListener("load", function(){
 
     }
     class Game{
-
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+            this.player = new Player();
+        }
+        update(){
+            this.player.update();
+        }
+        draw(context){
+            this.player.draw(context);
+        }
     }
+    const game = new Game(canvas.width, canvas.height);
 })
